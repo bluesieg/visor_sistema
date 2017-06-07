fe87e52310676218e7c356d9abc5fe1fe6346432
VLADY <VLADY@DESKTOP-TRGQ0A6>
2017-06-07 16:14:19 -0500

    autocomplete nomvia_cod_via global

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 Route::get('/', function () {
     return view("welcome");
 });
@@ -17,12 +18,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/vw_general', 'General@index')->name('vw_general');
 
-Route::get('msg',function(){
+Route::get('msg', function() {
     return view('fnewUsuario');
 });
 
 
-Route::get('via','General@autocompletar_direccion');
+Route::get('via', 'General@autocompletar_direccion');
 
 //Route::get('/usuarios', function () {
 //    return view("administracion/vw_usuarios");
@@ -30,39 +31,52 @@ Route::get('via','General@autocompletar_direccion');
 
 
 
-Route::group(['middleware' => 'auth'], function(){
-    
+Route::group(['middleware' => 'auth'], function() {
+
     Route::get('list_usuarios', 'Usuarios@index'); // tabla grilla Usuarios
-    Route::get('/usuarios','Usuarios@getAllUsuarios')->name('usuarios');//vw_usuarios
-    
-    Route::post('usuario_save','Usuarios@insert_update_Usuarios');
-    Route::post('usuario_delete','Usuarios@eliminar_usuario');//eliminar usuario
-    
-    /****************************AUTOLLENADO DE COMBOS**********************************************************************/
-    Route::get('get_all_tipo_documento','General@get_tipo_doc');//llena combo tipo documento
-    Route::get('get_all_cond_exonerac','General@get_cond_exonerac');//llena combo condicion exonerac
-    Route::get('autocompletar_direccion','General@autocompletar_direccion');//autocompleta el input text avenida,jiro, calle de contribuyentes
-    /*********************DEPARTAMENTO  PROVINCIA DISTRITO  *****************************************************************/
-    Route::get('get_all_dpto','General@get_dpto');//llena combo departamentos
-    Route::get('get_all_prov','General@get_prov');//llena combo provincias
-    Route::get('get_all_dist','General@get_dist');//llena combo Distritos
-    /************************************************************************************************************************/
-    /****************************************CONTRIBUYENTES*********************************************************************/
-    Route::get('contribuyentes','adm_tributaria\Contribuyentes@vw_contribuyentes')->name('adm_contribuyentes');// VW_CONTRIUYENTES
+    Route::get('/usuarios', 'Usuarios@getAllUsuarios')->name('usuarios'); //vw_usuarios
+
+    Route::post('usuario_save', 'Usuarios@insert_update_Usuarios');
+    Route::post('usuario_delete', 'Usuarios@eliminar_usuario'); //eliminar usuario
+
+    /*     * **************************AUTOLLENADO DE COMBOS********************************************************************* */
+    Route::get('get_all_tipo_documento', 'General@get_tipo_doc'); //llena combo tipo documento
+    Route::get('get_all_cond_exonerac', 'General@get_cond_exonerac'); //llena combo condicion exonerac
+    Route::get('autocompletar_direccion', 'General@autocompletar_direccion'); //autocompleta el input text avenida,jiro, calle de contribuyentes
+    /*     * *******************DEPARTAMENTO  PROVINCIA DISTRITO  **************************************************************** */
+    Route::get('get_all_dpto', 'General@get_dpto'); //llena combo departamentos
+    Route::get('get_all_prov', 'General@get_prov'); //llena combo provincias
+    Route::get('get_all_dist', 'General@get_dist'); //llena combo Distritos
+    /*     * ********************************************************************************************************************* */
+    /*     * **************************************CONTRIBUYENTES******************************************************************** */
+    Route::get('contribuyentes', 'adm_tributaria\Contribuyentes@vw_contribuyentes')->name('adm_contribuyentes'); // VW_CONTRIUYENTES
     Route::get('grid_contribuyentes', 'adm_tributaria\Contribuyentes@index'); // tabla grilla Contribuyentes    
-    Route::get('llenar_form_contribuyentes','adm_tributaria\Contribuyentes@llenar_form_contribuyentes');//llena form contribuyentes
-    Route::post('insert_new_contribuyente','adm_tributaria\Contribuyentes@insert_new_contribuyente');
-    Route::post('contribuyente_update','adm_tributaria\Contribuyentes@modificar_contribuyente');//update contribuyente
-    Route::post('contribuyente_delete','adm_tributaria\Contribuyentes@eliminar_contribuyente');//eliminar contribuyente
-    /****************************************VALORES ARANCELARIOS*********************************************************************/
-    Route::get('val_arancel','configuracion\Valores_Arancelarios@vw_val_arancel')->name('val_aran');// VW_CONTRIUYENTES
-    Route::get('grid_val_arancel', 'configuracion\Valores_Arancelarios@grid_valores_arancelarios'); // tabla grilla Contribuyentes
-    Route::get('get_anio_val_arancel','configuracion\Valores_Arancelarios@get_anio');//llena combo AÑO vw_val_arancel
-    Route::get('get_sector_val_arancel','configuracion\Valores_Arancelarios@get_sector');//llena combo SECTOR vw_val_arancel
-    Route::get('get_mzna_val_arancel','configuracion\Valores_Arancelarios@get_mzna');//llena combo MANZANAvw_val_arancel
-    Route::get('val_arancek_autocomplete_nom_via','configuracion\Valores_Arancelarios@get_autocomplete_nom_via');//autocompletar arancel cod_via->nom_completo de via
-    Route::post('insert_valor_arancel','configuracion\Valores_Arancelarios@insert_valor_arancel');
-    Route::post('update_valor_arancel','configuracion\Valores_Arancelarios@update_valor_arancel');
-    Route::resource('predios_urbanos','adm_tributaria\PredioController');
+    Route::get('llenar_form_contribuyentes', 'adm_tributaria\Contribuyentes@llenar_form_contribuyentes'); //llena form contribuyentes
+    Route::post('insert_new_contribuyente', 'adm_tributaria\Contribuyentes@insert_new_contribuyente');
+    Route::post('contribuyente_update', 'adm_tributaria\Contribuyentes@modificar_contribuyente'); //update contribuyente
+    Route::post('contribuyente_delete', 'adm_tributaria\Contribuyentes@eliminar_contribuyente'); //eliminar contribuyente
+
+    /*     * ******************************************VALORES ARANCELARIOS******************************************************************** */
+    Route::group(['namespace' => 'configuracion'], function() {
+        Route::get('val_arancel', 'Valores_Arancelarios@vw_val_arancel')->name('val_aran'); // VW_ARANCELES
+        Route::get('grid_val_arancel', 'Valores_Arancelarios@grid_valores_arancelarios'); // tabla grilla Valores Arancelarios
+        Route::get('get_anio_val_arancel', 'Valores_Arancelarios@get_anio'); //llena combo AÑO vw_val_arancel
+        Route::get('get_sector_val_arancel', 'Valores_Arancelarios@get_sector'); //llena combo SECTOR vw_val_arancel
+        Route::get('get_mzna_val_arancel', 'Valores_Arancelarios@get_mzna'); //llena combo MANZANAvw_val_arancel
+        Route::get('autocomplete_nom_via', 'Valores_Arancelarios@get_autocomplete_nom_via'); //autocompletar arancel cod_via->nom_completo de via
+        Route::post('insert_valor_arancel', 'Valores_Arancelarios@insert_valor_arancel');
+        Route::post('update_valor_arancel', 'Valores_Arancelarios@update_valor_arancel');
+        Route::post('delete_valor_arancel', 'Valores_Arancelarios@delete_valor_arancel');
+    });
+
+    /*     * ****************************************   VALORES UNITARIOS    ************************************************************** */
     
+    Route::group(['namespace' => 'configuracion'], function() {
+        Route::get('val_unit', 'Valores_Unitarios@show_vw_val_unit')->name('valores_unitarios'); // VW_VALORES_UNITARIOS
+        Route::get('grid_val_unitarios', 'Valores_Unitarios@grid_val_unitarios'); // tabla grilla VALORES UNITARIOS
+    });
+
+
+    Route::resource('predios_urbanos', 'adm_tributaria\PredioController');
 });
+
