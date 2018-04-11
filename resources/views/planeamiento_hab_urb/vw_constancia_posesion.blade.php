cde98b15016c806178e1f851c1b963382d99bdcb
anjim <anjim@DESKTOP-OUI49JP>
2018-04-11 12:25:49 -0500

    commit1

--- resources/views/planeamiento_hab_urb/vw_constancia_posesion.blade.php
+++ resources/views/planeamiento_hab_urb/vw_constancia_posesion.blade.php
@@ -1,12 +1,12 @@
 //@extends('layouts.map')
 @section('content')
-
-            <section id="widget-grid" class="" style="padding-top: 5px; ">
-               <div class="row">                    
+<section id="widget-grid" class="">    
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-bottom: -12px">
+            <div class="well well-sm well-light">
+                <div class="row">                    
                     
                     <section class="col col-lg-12">
-                       
-                        <hr style="border: 1px solid #DDD;margin: 10px -10px"> 
                         
                         <ul id="tabs1" class="nav nav-tabs bordered">
                             <li class="active">
@@ -28,72 +28,50 @@
                       <div id="s1" class="tab-pane fade active in">
                         <section class="col col-lg-12">
                         <div class="col-xs-12">               
-                           <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-bottom: -12px;padding-right: 5px;">
-                        <div class="well well-sm well-light">
-                            <h1 class="txt-color-green"><b>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <section style="padding-right: 10px">
+                                        <div class="col-xs-12">
+                                            
                                     <h1 class="txt-color-green"><b>REGISTRO DE EXPEDIENTES</b></h1>
-                            <div class="row">
-                                
-                                
-                               
-                                
-                                
-                                <div class="col-xs-12">
-                                     <div class="col-lg-3" style="padding-right: 5px; padding-top: 20px; font-size: 0.6em">
-                                    <div class="input-group input-group-md">
-                                        <span class="input-group-addon">Desde:</span>
-                                        <div class="icon-addon addon-md">
-                                        <input  id="fec_ini_cajas" name="dlg_fec" type="text"   class="datepicker text-center" data-dateformat='dd/mm/yy' data-mask="99/99/9999" style="height: 32px; width: 100%" placeholder="--/--/----" value="{{date('d/m/Y')}}">
+                                        <div class="col-lg-3" style="padding-right: 5px; padding-top: 20px; font-size: 0.6em">
+                                           <div class="input-group input-group-md">
+                                               <span class="input-group-addon">Desde:</span>
+                                               <div class="icon-addon addon-md">
+                                               <input  id="fec_ini_cajas" name="dlg_fec" type="text"   class="datepicker text-center" data-dateformat='dd/mm/yy' data-mask="99/99/9999" style="height: 32px; width: 100%" placeholder="--/--/----" value="{{date('d/m/Y')}}">
+                                               </div>
+                                           </div>
+                                       </div>
+                                       <div class="col-lg-3" style="padding-right: 5px; padding-top: 20px; font-size: 0.6em">
+                                            <div class="input-group input-group-md">
+                                                <span class="input-group-addon">Hasta:</span>
+                                                <div class="icon-addon addon-md">
+                                                <input id="fec_fin_cajas" name="dlg_fec" type="text"   class="datepicker text-center" data-dateformat='dd/mm/yy' data-mask="99/99/9999" style="height: 32px; width: 100%" placeholder="--/--/----" value="{{date('d/m/Y')}}">
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-3" style="padding-right: 5px; padding-top: 20px; font-size: 0.6em">
-                                    <div class="input-group input-group-md">
-                                        <span class="input-group-addon">Hasta:</span>
-                                        <div class="icon-addon addon-md">
-                                        <input id="fec_fin_cajas" name="dlg_fec" type="text"   class="datepicker text-center" data-dateformat='dd/mm/yy' data-mask="99/99/9999" style="height: 32px; width: 100%" placeholder="--/--/----" value="{{date('d/m/Y')}}">
+                                       <div class="text-right" style=" padding-top: 20px">
+
+                                                <button type="button" class="btn btn-labeled bg-color-greenLight txt-color-white" onclick="nuevo_tim();">
+                                                   <span class="btn-label"><i class="glyphicon glyphicon-plus-sign"></i></span>Nuevo
+                                               </button>
+
+                                               <button  type="button" class="btn btn-labeled bg-color-blue txt-color-white" onclick="actualizar_tim();">
+                                                   <span class="btn-label"><i class="glyphicon glyphicon-pencil"></i></span>Modificar
+                                               </button>
+
+                                           <button  type="button" class="btn btn-labeled btn-danger" onclick="eliminar_tim();">
+                                               <span class="btn-label"><i class="glyphicon glyphicon-trash"></i></span>Eliminar
+                                           </button>
+
+
+                                       </div>
                                         </div>
-                                    </div>
-                                </div>
-                                    <div class="text-right" style=" padding-top: 20px">
-                                      
-                                             <button type="button" class="btn btn-labeled bg-color-greenLight txt-color-white" onclick="nuevo_tim();">
-                                                <span class="btn-label"><i class="glyphicon glyphicon-plus-sign"></i></span>Nuevo
-                                            </button>
-                                       
-                                            <button  type="button" class="btn btn-labeled bg-color-blue txt-color-white" onclick="actualizar_tim();">
-                                                <span class="btn-label"><i class="glyphicon glyphicon-pencil"></i></span>Modificar
-                                            </button>
-                                       
-                                        <button  type="button" class="btn btn-labeled btn-danger" onclick="eliminar_tim();">
-                                            <span class="btn-label"><i class="glyphicon glyphicon-trash"></i></span>Eliminar
-                                        </button>
-                                       
-                                      
-                                    </div>
+                                    </section>
+                                   
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                            
-                            <div class="row">
-                                
-                                <div class="col-xs-4" style="padding-left: 80px;">
-                                    <div class="input-group input-group-md">
-                                        <span class="input-group-addon">Monto. &nbsp;<i class="fa fa-hashtag"></i></span>
-                                        <div class=""  >
-                                            <input id="dlg_monto" type="text" placeholder="INGRESE MONTO" class="form-control input-sm text-uppercase text-center" style="height: 32px;" onkeypress="return soloNumeroTab(event);">
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-xs-1">
-                                    
-                            </div>
-                            </div>    
-                            
-                            
-                        </div>
+                           </div>
                         </section>
                         
                       </div>
@@ -146,11 +124,10 @@
       
                     </section>
                 </div>
-            </section>
-
-
+            </div>            
+        </div>       
+    </div>
 </section>
-
 <script language="JavaScript" type="text/javascript" src="{{ asset('archivos_js/planeamiento_hab_urb/registro_expendientes.js') }}"></script>
 <div id="dlg_nuevo_tim" style="display: none;">
     <div class='cr_content col-xs-12 ' style="margin-bottom: 10px;">
