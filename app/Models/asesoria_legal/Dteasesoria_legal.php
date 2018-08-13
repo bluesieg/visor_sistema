8cbcd58797e350c838ef625ad28af160cd898621
anjim <anjim@DESKTOP-OUI49JP>
2018-08-13 09:36:51 -0500

    asesoria 4

--- app/Models/asesoria_legal/Dteasesoria_legal.php
+++ app/Models/asesoria_legal/Dteasesoria_legal.php
@@ -8,6 +8,6 @@ class Dteasesoria_legal extends Model
 {
     protected $connection = 'gerencia_catastro';
     public $timestamps = false;
-    protected $table = 'asesoria_legal.dte_procuraduria';
+    protected $table = 'asesoria_legal.dte_asesoria_legal';
     protected $primaryKey='id_det_asesoria_legal';
 }
